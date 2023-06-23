c26255fef410a68335d504ede718aba3742e948a
muz <muiz-erem@gmx.de>
2023-06-23 18:34:44 +0200

    1.2 sol

--- solutions/1-2-solution.cpp
+++ solutions/1-2-solution.cpp
@@ -2,22 +2,28 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
 
 int main() {
   auto input = std::fstream("../input/input1.txt");
-  auto max_cals = 0;
+  auto vec = std::vector<int>();
 
   auto current_cals = 0;
   for (std::string line; std::getline(input, line);) {
     if (line.empty()) {
-      max_cals = std::max(max_cals, current_cals);
+      vec.push_back(std::max(current_cals, current_cals));
       current_cals = 0;
     } else {
       current_cals += std::stoi(line);
     }
   }
 
-  std::cout << max_cals << std::endl;
+  std::sort(vec.begin(), vec.end());
+
+  auto SumOfTopThree =
+      vec[vec.size() - 1] + vec[vec.size() - 2] + vec[vec.size() - 3];
+
+  std::cout << SumOfTopThree << std::endl;
 
   return 0;
 }
