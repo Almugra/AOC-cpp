341562e96b4c94ca1dba4a1067fcae03a5ee790c
muz <muiz-erem@gmx.de>
2023-06-27 14:00:27 +0200

    util.h

--- utils/util.h
+++ utils/util.h
@@ -1,27 +1,27 @@
-#ifndef UTIL
-#define UTIL
+#ifndef UTIL_H
+#define UTIL_H
 
 #include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
-vector<string> splitByDelimString(string s, char d) {
+vector<string> splitByDelimString(const string &s, char d) {
   auto ss = stringstream(s);
   auto v = vector<string>();
   for (string tmp; getline(ss, tmp, d);) {
-    v.push_back(tmp);
+    v.emplace_back(tmp);
   }
   return v;
 }
 
-vector<int> splitByDelimInt(string s, char d) {
+vector<int> splitByDelimInt(const string &s, char d) {
   auto ss = stringstream(s);
   auto v = vector<int>();
   for (string tmp; getline(ss, tmp, d);) {
-    v.push_back(stoi(tmp));
+    v.emplace_back(stoi(tmp));
   }
   return v;
 }
 
-#endif // !UTIL
+#endif // !UTIL_H
